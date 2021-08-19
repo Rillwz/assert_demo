7d17e666d53bec6b7290b38d5500b1ff522b65ea
Yusril Ihsanul Alim <73714271+Rillwz@users.noreply.github.com>
2021-08-19 18:24:25 +0700

    first commit

---
+++ bin/assert_demo.dart
@@ -0,0 +1,24 @@
+import 'package:assert_demo/assert_demo.dart' as assert_demo;
+
+void main(List<String> arguments) {
+  Person p;
+
+  try {
+    p = Person(name: 'Yusril');
+  } catch (e) {
+    print(e);
+  }
+
+  print('hello');
+}
+
+class Person {
+  final String name;
+  final int age;
+
+  Person({required this.name, this.age = 0}) {
+    // assertion error! if given value doesn't matches!
+    // ignore: unnecessary_null_comparison
+    assert(name != null, 'You must give the person name');
+  }
+}
